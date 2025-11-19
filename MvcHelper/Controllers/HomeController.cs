3911b186d6bd9399d318db84a1fad04fbba547a7
SumaiyaNishat <sumaiyanishat94@gmail.com>
2025-11-19 11:45:43 +0600

    Add project files.

---
+++ MvcHelper/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MvcHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(User user)
+        {
+            TempData["Message"] = "User " + user.Name + " saved successfully!";
+            return RedirectToAction("About");
+        }
+        
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
